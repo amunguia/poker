5cb5497e0df281ed7add4dd2b7a73846f9555edd
Alex <munguia.jandro@gmail.com>
2014-11-05 18:48:58 -0800

    Add starting balance

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+  STARTING_BALANCE = 10000
+  
   attr_accessor :password, :password_confirmation
   before_save :encrypt_password
+  before_save :initial_balance
   
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
@@ -21,6 +24,10 @@ class User < ActiveRecord::Base
       self.password_salt = BCrypt::Engine.generate_salt
       self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
     end
+  end
+  
+  def initial_balance
+    self.balance = STARTING_BALANCE
   end
   
 
