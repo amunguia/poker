275589ea4212c6df34b60dc729515fdf565b0233
Alex <munguia.jandro@gmail.com>
2014-11-05 17:57:40 -0800

    Add basic static pages with login/logout workflow

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Poker::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  get "about" => "static_pages#about", :as => "about"
+  get "logged_in" => "static_pages#logged_in", :as => "logged_in"
+  root :to => "static_pages#home"
   resources :users
   resources :sessions
   
