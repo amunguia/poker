bf916c3f65cd7e376f9805303038be0a6e908c45
Alejandro Munguia <munguia.jandro@gmail.com>
2014-11-17 16:48:01 -0600

    Patch so db:migrate runs

--- config/initializers/create_rooms.rb
+++ config/initializers/create_rooms.rb
@@ -1,40 +1,40 @@
 #This initializer will create the rooms and tables that form the basic website.
 
-if !Room.find_by_name("Basic Room")
+#if !Room.find_by_name("Basic Room")
 
-  basic_room = Room.new do |r|
-    r.name = "Basic Room"
-    r.minimum_bet = 5
-  end
-  basic_room.save
+ # basic_room = Room.new do |r|
+  #  r.name = "Basic Room"
+  #  r.minimum_bet = 5
+  #end
+  #basic_room.save
 
-  medium_room = Room.new do |r|#
-    r.name = "Medium Room"
-    r.minimum_bet = 20
-  end
-  medium_room.save
+  #medium_room = Room.new do |r|#
+  #  r.name = "Medium Room"
+  #  r.minimum_bet = 20
+  #end
+  #medium_room.save
 
-  high_roller_room = Room.new do |r|
-    r.name = "High Rollers Room"
-    r.minimum_bet = 50
-  end
-  high_roller_room.save
+  #high_roller_room = Room.new do |r|
+  #  r.name = "High Rollers Room"
+  #  r.minimum_bet = 50
+  #end
+  #high_roller_room.save
 
 
-  (1..10).each do |i|
-    Table.new do |t|
-      t.number = i
-      t.room = basic_room  
-    end.save
+  #(1..10).each do |i|
+  #  Table.new do |t|
+  #    t.number = i
+  #    t.room = basic_room  
+  #  end.save
 
-    Table.new do |t|
-      t.number = i
-      t.room = medium_room
-    end.save
+  #  Table.new do |t|
+  #    t.number = i
+  #    t.room = medium_room
+  #  end.save
 
-    Table.new do |t|
-      t.number = i
-      t.room = high_roller_room
-    end.save
-  end
-end
+   # Table.new do |t|
+  #    t.number = i
+  #    t.room = high_roller_room
+  #  end.save
+  #end
+#end
