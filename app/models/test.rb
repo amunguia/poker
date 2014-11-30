23ff2465c782951addcfd0a9b7d1321c8d3a16d0
Dong <dongli456@yahoo.com>
2014-11-30 10:47:03 -0800

    make change straight

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,19 +4,22 @@ class Test
 
 
 def main
+puts "testing -------------"
 cards = Array.new
-cards[0] = Card.new('1','H');
-cards[1] = Card.new('1','H');
-cards[2] = Card.new('9','H');
-cards[3] = Card.new('4','H');
-cards[4] = Card.new('5','H');
+cards[0] = Card.new('10','H');
+cards[1] = Card.new('11','H');
+cards[2] = Card.new('12','H');
+cards[3] = Card.new('13','H');
+cards[4] = Card.new('1','H');
 cards[5] = Card.new('4','S');
-cards[6] = Card.new('7','S');
+cards[6] = Card.new('4','S');
 
 
 myhand = Hand.new(cards);
-
 myhand.best_hand
+
+myhand.printcard(myhand.besthand)
+puts "score : #{myhand.bestscore}"
 
 
 
