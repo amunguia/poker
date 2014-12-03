bab233bfedb5e269c11381a2ad2d170fb763afa6
Alejandro Munguia <munguia.jandro@gmail.com>
2014-12-03 14:46:28 -0800

    create_rooms.rb uncommented

--- config/initializers/create_rooms.rb
+++ config/initializers/create_rooms.rb
@@ -1,54 +1,48 @@
 #This initializer will create the rooms and tables that form the basic website.
 
-#if !Room.find_by_name("Basic Room")
+if !Room.find_by_name("Basic Room")
 
- # basic_room = Room.new do |r|
- #   r.name = "Basic Room"
- #   r.minimum_bet = 5
- # end
- # basic_room.save
+  basic_room = Room.new do |r|
+    r.name = "Basic Room"
+    r.minimum_bet = 5
+  end
+  basic_room.save
 
- # medium_room = Room.new do |r|#
- #   r.name = "Medium Room"
- #   r.minimum_bet = 20
- # end
- # medium_room.save
+  medium_room = Room.new do |r|#
+    r.name = "Medium Room"
+    r.minimum_bet = 20
+  end
+  medium_room.save
 
-  #high_roller_room = Room.new do |r|
-  #  r.name = "High Rollers Room"
-  #  r.minimum_bet = 50
-  #end
-  #high_roller_room.save
+  high_roller_room = Room.new do |r|
+    r.name = "High Rollers Room"
+    r.minimum_bet = 50
+  end
+  high_roller_room.save
 
 
-  #(1..10).each do |i|
-  #  Table.new do |t|
-  #    t.number = i
-  #    t.room = basic_room  
-#<<<<<<< HEAD
-#      t.min_bet = basic_room.minimum_bet
-#=======
-#>>>>>>> dong
- #   end.save
+  (1..10).each do |i|
+    Table.new do |t|
+      t.number = i
+      t.room = basic_room  
+      t.min_bet = basic_room.minimum_bet
+    end.save
 
-#    Table.new do |t|
-#      t.number = i
-#      t.room = medium_room
-#<<<<<<< HEAD
-#      t.min_bet = medium_room.minimum_bet
-#    end.save
+    Table.new do |t|
+      t.number = i
+      t.room = medium_room
+      t.min_bet = medium_room.minimum_bet
+    end.save
 
-#   Table.new do |t|
-#      t.number = i
-#      t.room = high_roller_room
-#      t.min_bet = high_roller_room.minimum_bet
-#=======
- #   end.save
-#
- #   Table.new do |t|
- #     t.number = i
-#     t.room = high_roller_room
-##>>>>>>> dong
- #   end.save
- # end
-#end
+   Table.new do |t|
+      t.number = i
+      t.room = high_roller_room
+      t.min_bet = high_roller_room.minimum_bet
+   end.save
+
+   Table.new do |t|
+      t.number = i
+      t.room = high_roller_room
+    end.save
+  end
+end
