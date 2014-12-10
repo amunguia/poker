465b11d09f46de8fe745d03f21b4c6cb88bafca4
Silvio Huebner <silviohs@mail.sfsu.edu>
2014-12-10 15:07:30 -0800

    redo buttons on tables view

--- app/assets/javascripts/view_panels.js
+++ app/assets/javascripts/view_panels.js
@@ -13,7 +13,7 @@ function show_game_started(started, winner_id) {
 }
 
 function new_game_panel() {
-    var html = '<button id="join-panel" class="btn btn-primary button">NEW GAME</button><br>';
+    var html = '<br><button id="join-panel" class="btn btn-primary observe-btn button">CONTINUE</button><br>';
     //html += '<button id="observe-panel" class="btn btn-danger button">WATCH</button><br>';
     poker.next_game = false;
 
