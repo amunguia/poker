86e8063c57b04a4aa2d807bf5be2819022a2acee
Silvio Huebner <silviohs@mail.sfsu.edu>
2014-12-09 21:01:23 -0800

    integrated chat window into game frame

--- app/assets/javascripts/view_panels.js
+++ app/assets/javascripts/view_panels.js
@@ -37,7 +37,7 @@ function build_game_center_panel() {
 		return; //Already there.
 	}
 
-    var html = '<div id="stream-panel" class="col-sm-7">';
+    var html = '<div id="stream-panel" class="col-md-8">';
     html += '<image src="../../cards/card_back.png" class="card-img" id="card1">';
     html += '<image src="../../cards/card_back.png" class="card-img" id="card2">';
     html += '<image src="../../cards/card_back.png" class="card-img" id="card3">';
